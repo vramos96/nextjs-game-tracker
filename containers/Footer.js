39068327a626a18273e815495e425dca116f11d6
Victor Ramos <vart96@hotmail.com>
2021-08-28 08:32:03 -0700

    Changed header and footer, added basic form features

--- containers/Footer.js
+++ containers/Footer.js
@@ -3,12 +3,18 @@ import styles from '../styles/Home.module.css'
 
 const Footer = () => {
     const poweredByUrl = "https://tracker.gg/"
+    const createdByUrl = "https://github.com/vramos96"
     return (
         <footer className={styles.footer}>
             <h3>
                 Powered by &nbsp;
                 <a target="_blank" href={poweredByUrl}>
                     {poweredByUrl}
+                </a>
+                <br />
+                Created by &nbsp;
+                <a target="_blank" href="#">
+                    {createdByUrl}
                 </a>
             </h3>
         </footer>
--- containers/Header.js
+++ containers/Header.js
@@ -1,22 +1,45 @@
 import React from 'react'
+import Image from 'next/image'
 import { Grid } from '@material-ui/core'
 import styles from '../styles/Home.module.css'
 
 const Header = () => {
-    const leftImageSrc = "https://logodownload.org/wp-content/uploads/2019/02/apex-legends-logo-5.png"
-    const appName = process?.env?.NEXT_PUBLIC_APP_NAME ?? "MY TRACKER"
-    const rightImageSrc = "https://upload.wikimedia.org/wikipedia/commons/thumb/5/55/Overwatch_circle_logo.svg/600px-Overwatch_circle_logo.svg.png"
+    const appName = process?.env?.NEXT_PUBLIC_APP_NAME ?? "MY TRACKER V2"
     return (
         <header className={styles.header}>
            <Grid container spacing={3}>
             <Grid item xs={4}>
-                <img width="100px" height="100px" src={leftImageSrc}></img>
+                <Image 
+                    src="/apex-legends-logo.png" 
+                    alt="Apex Legends Logo" 
+                    width={100} 
+                    height={100} 
+                />
+                <Image 
+                    src="/octane-image.jpg" 
+                    alt="Apex Legends Logo" 
+                    width={200} 
+                    height={100} 
+                />
             </Grid>
             <Grid item xs={4}>
-                <h1>{ appName }</h1>
+                <h1>
+                    { appName }
+                </h1>
             </Grid>
             <Grid item xs={4}>
-                <img width="100px" height="100px" src={rightImageSrc}></img>
+                <Image 
+                    src="/roadhog-image.png" 
+                    alt="Apex Legends Logo" 
+                    width={100}
+                    height={100} 
+                />
+                <Image 
+                    src="/overwatch-logo.png" 
+                    alt="Apex Legends Logo" 
+                    width={100}
+                    height={100}
+                />
             </Grid>
            </Grid>
         </header>
